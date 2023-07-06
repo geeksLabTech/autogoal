8fcff4fa26a99fb64977bec8038408299ea632fc
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2023-07-06 02:35:16 -0400

    dockerfile not working

--- dockerfiles/install-package.sh
+++ dockerfiles/install-package.sh
@@ -10,19 +10,19 @@ do
     # take core build out
     case $arg in 
         core)
-            cd /home/coder/autogoal/autogoal && poetry install
+            cd /home/coder/autogoal/autogoal && poetry install --no-dev --no-interaction --no-root
             cd /home/coder/autogoal && pip install -e autogoal
         ;;
         remote)
-            cd /home/coder/autogoal/autogoal-remote && poetry install
+            cd /home/coder/autogoal/autogoal-remote && poetry install --no-dev --no-interaction --no-root
             cd /home/coder/autogoal && pip install -e autogoal-remote
         ;;
         common)
-            cd /home/coder/autogoal/autogoal-contrib/autogoal_contrib && poetry install
+            cd /home/coder/autogoal/autogoal-contrib/autogoal_contrib && poetry install --no-dev --no-interaction --no-root
             cd /home/coder/autogoal/autogoal-contrib && pip install -e autogoal_contrib
         ;;
         *)
-            cd "/home/coder/autogoal/autogoal-contrib/autogoal_$arg" && poetry install
+            cd "/home/coder/autogoal/autogoal-contrib/autogoal_$arg" && poetry install --no-dev --no-interaction --no-root
             cd "/home/coder/autogoal/autogoal-contrib" && pip install -e "autogoal_$arg"
         ;;
     esac
