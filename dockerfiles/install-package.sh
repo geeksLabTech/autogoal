dde641de7ff9384e7b463e7f59c152b0d5cb37ca
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2023-07-15 02:17:12 -0400

    Update install-package.sh

--- dockerfiles/install-package.sh
+++ dockerfiles/install-package.sh
@@ -10,19 +10,19 @@ do
     # take core build out
     case $arg in 
         core)
-            cd /home/coder/autogoal/autogoal && poetry install --no-interaction --no-root
+            cd /home/coder/autogoal/autogoal && poetry install --no-interaction 
             cd /home/coder/autogoal && pip install -e autogoal 
         ;;
         remote)
-            cd /home/coder/autogoal/autogoal-remote && poetry install --no-root --no-interaction
+            cd /home/coder/autogoal/autogoal-remote && poetry install --no-interaction
             cd /home/coder/autogoal && pip3 install -e autogoal-remote
         ;;
         common)
-            cd /home/coder/autogoal/autogoal-contrib/autogoal_contrib && poetry install --no-root --no-interaction
+            cd /home/coder/autogoal/autogoal-contrib/autogoal_contrib && poetry install --no-interaction
             cd /home/coder/autogoal/autogoal-contrib && pip3 install -e autogoal_contrib
         ;;
         *)
-            cd "/home/coder/autogoal/autogoal-contrib/autogoal_$arg" && poetry install --no-root --no-interaction
+            cd "/home/coder/autogoal/autogoal-contrib/autogoal_$arg" && poetry install --no-interaction
             cd "/home/coder/autogoal/autogoal-contrib" && pip3 install -e "autogoal_$arg"
         ;;
     esac
